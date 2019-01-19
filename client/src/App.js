3287576d4a698efa2d2ec81f38827ef427765642
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-01-19 14:03:16 -0600

    React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./views/Home";
+import Services from "./views/Services";
+import Contact from "./views/Contact";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Route exact path="/" component={Home} />
+				<Route path="/services" component={Services} />
+				<Route path="/contact" component={Contact} />
+			</div>
+		);
+	}
 }
 
 export default App;
